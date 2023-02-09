eb798928f9a3d0a9043036907c9e730f0ad575ea
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-09 18:54:35 +0530

    Update ItemsController.cs

--- src/Catalog.API/Controllers/ItemsController.cs
+++ src/Catalog.API/Controllers/ItemsController.cs
@@ -43,11 +43,11 @@ public class ItemsController : ControllerBase
     {
         var existingItem = items.Where(item => item.Id == id).SingleOrDefault();
 
-        if(existingItem is null)
+        if (existingItem is null)
         {
             return NotFound();
         }
-        
+
         var updatedItem = existingItem with
         {
             Name = updateItemDto.Name,
@@ -66,6 +66,12 @@ public class ItemsController : ControllerBase
     public IActionResult Delete(Guid id)
     {
         var index = items.FindIndex(existingItem => existingItem.Id == id);
+
+        if (index < 0)
+        {
+            return NotFound();
+        }
+
         items.RemoveAt(index);
 
         return NoContent();
