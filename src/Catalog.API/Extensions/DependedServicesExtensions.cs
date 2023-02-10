a4ae5308efb981b9e3a5a4a2ef460dc9fd7adae7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-10 13:51:30 +0530

    DependedServicesExtensions

---
+++ src/Catalog.API/Extensions/DependedServicesExtensions.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+
+namespace Catalog.API.Extensions;
+
+
+public static class DependedServicesExtensions
+{
+
+    public static IServiceCollection ConfigureDependedServices(this IServiceCollection services)
+    {
+        BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+        BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+        _ = services.AddControllers(options =>
+        {
+            options.SuppressAsyncSuffixInActionNames = false;
+        });
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        _ = services.AddEndpointsApiExplorer();
+        _ = services.AddSwaggerGen();
+
+        _ = services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll", policy => policy.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+        });
+
+        return services;
+    }
+
+}
--- src/Catalog.API/Program.cs
+++ src/Catalog.API/Program.cs
@@ -1,21 +1,9 @@
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.Serializers;
+using Catalog.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-
-builder.Services.AddControllers(options =>
-            {
-                options.SuppressAsyncSuffixInActionNames = false;
-            });
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.ConfigureDependedServices();
 
 var app = builder.Build();
 
