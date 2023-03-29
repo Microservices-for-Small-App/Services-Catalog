38d5754aa8fa24373434887e82273904cac8fb99
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-03-29 21:47:17 +0530

    Update CatalogItem.cs

--- src/Catalog.Data/Entities/CatalogItem.cs
+++ src/Catalog.Data/Entities/CatalogItem.cs
@@ -6,9 +6,9 @@ public class CatalogItem : IEntity
 {
     public Guid Id { get; set; }
 
-    public string Name { get; set; } = string.Empty;
+    public string? Name { get; set; }
 
-    public string Description { get; set; } = string.Empty;
+    public string? Description { get; set; }
 
     public decimal Price { get; set; }
 
